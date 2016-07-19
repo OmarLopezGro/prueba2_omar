165438814b091b8959773f4537cb71cb80d93424
OmarLopezGro <omallogu@gmail.com>
2016-07-18 22:00:36 -0500

    Septimo commite, se agregaron scopes

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,4 +1,6 @@
 class Inventory < ActiveRecord::Base
 	belongs_to :operations
 	has_one :operation
+
+	scope :inventario_primer_operador, → {where (:inventory_id =>1)}  
 end
--- app/models/operation.rb
+++ app/models/operation.rb
@@ -2,4 +2,7 @@ class Operation < ActiveRecord::Base
 	belongs_to :worker
 	belongs_to :inventory
 	has_many :inventories
+
+	scope :Ultimos5, -> {limit(5).order(:id => :desc) }
+	scope :Ultimo_dia, lambda {where('pubished_at >= ?', Time.now - 1.day)}
 end
